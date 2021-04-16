c64b14784802aaeac16a6741b90b08a6528da268
unknown <ibmjavanh05@iiht.tech>
2021-04-16 17:09:05 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserformComponent } from './userform/userform.component';
@@ -12,7 +13,7 @@ import { UserformComponent } from './userform/userform.component';
     UserformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
